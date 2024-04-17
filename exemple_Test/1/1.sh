d61546741a992c091e4d9e94a12c8977e7dcd981
H <h@H-Kali.lan>
2024-04-17 20:19:07 +0300

    extra

---
+++ exemple_Test/1/1.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+if [[ $# -lt 1 ]]; then
+	echo "Utilizare ./1.sh file1 file2 ..."
+	exit 0 
+fi
+
+read var
+while [ $var != "stop" ]; do
+	for file in $@; do
+		found=$(find $file -type f -exec file {} + 2>/dev/null | grep "text" | awk '{ print $1}' | sed "s/://")
+		if [ $found ]; then
+			echo "$found"
+			number=$(cat $found | grep -Ec "$var")
+			echo "nr linii:$number"
+			if [ $(($number % 2)) -eq 0 ]; then
+				echo "$( cat $found | grep -E "$var" | tail -n 1)" >> $found
+			fi
+		else
+			echo "fisierul $file nu este de tip text"
+		fi
+	done
+	read var
+
+done;
---
+++ exemple_Test/1/2.sh
@@ -0,0 +1,5 @@
+#include /bin/bash
+
+while read -r line;do
+	echo "$line"
+done<"a.txt"
